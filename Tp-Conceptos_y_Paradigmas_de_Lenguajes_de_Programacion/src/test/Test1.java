408c298cbbe39a8c68ff271fdd890918f1e88f2c
Tomas Nieto <tnieto852@gmail.com>
2022-06-10 22:59:27 -0300

    Test 1 y 2 Completos

--- Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test1.java
+++ Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test1.java
@@ -18,33 +18,13 @@ public class Test1 {
 		Enfermedad miopiaPadre = new Miopia("Diabetes", 98.16f, 80.45f, 70.25f);
 		
 		
-		/*--- ENFERMEDADES MADRE ---*/
-		Enfermedad asmaMadre = new Asma("Asma", 33.12f, "Intermitente");
-		
-		Enfermedad diabetesMadre = new Diabetes("Diabetes", 2.45f, '1');
-		
-		Enfermedad miopiaMadre = new Miopia("Diabetes", 98.16f, 80.45f, 70.25f);
-		
-		
 		/*--- PADRE ---*/
 		Persona padre = new Persona("Raul", "Gomez", 55);
 		
 		padre.getEnfermedades().add(asmaPadre);
 		padre.getEnfermedades().add(diabetesPadre);
 		padre.getEnfermedades().add(miopiaPadre);
-
-		
-		/*--- MADRE ---*/
-		Persona madre = new Persona("Alejandra", "Martinez", 52);
-		
-		madre.getEnfermedades().add(asmaMadre);
-		madre.getEnfermedades().add(diabetesMadre);
-		madre.getEnfermedades().add(miopiaMadre);
 	
-		
-		/*--- HIJA ---*/
-		Persona hija = new Persona("Sofia", "Gomez", 18);
-		
 	}
 
 }
--- Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test2.java
+++ Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test2.java
@@ -1,11 +1,30 @@
 package test;
 
+import modelo.Asma;
+import modelo.Diabetes;
+import modelo.Enfermedad;
+import modelo.Miopia;
+import modelo.Persona;
+
 public class Test2 {
 
 	public static void main(String[] args) {
 
-
-
+		/*--- ENFERMEDADES MADRE ---*/
+		Enfermedad asmaTia = new Asma("Asma", 2.19f, "Intermitente");
+		
+		Enfermedad diabetesTia= new Diabetes("Diabetes", 78.45f, '1');
+		
+		Enfermedad miopiaTia = new Miopia("Diabetes", 0.70f, 0.45f, 0.25f);
+		
+		
+		/*--- MADRE ---*/
+		Persona tia = new Persona("Alejandra", "Martinez", 52);
+		
+		tia.getEnfermedades().add(asmaTia);
+		tia.getEnfermedades().add(diabetesTia);
+		tia.getEnfermedades().add(miopiaTia);
+	
 	}
 
 }
