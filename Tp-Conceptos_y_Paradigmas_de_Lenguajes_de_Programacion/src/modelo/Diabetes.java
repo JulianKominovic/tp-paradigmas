49a8b7ae4ebe82a0983a0b62e926426fb20a559d
Agusflh167 <73241721+Agusflh167@users.noreply.github.com>
2022-06-15 23:15:43 -0300

    Update Diabetes.java

--- Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/modelo/Diabetes.java
+++ Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/modelo/Diabetes.java
@@ -24,4 +24,23 @@ public class Diabetes extends Enfermedad {
 	public float calcularRiesgo() {
 		return 0;
 	}
+	public String calcularRiesgo(float nivelRP, float nivelRM, boolean test) {
+
+		String probabilidades = null;
+
+		if (test == true) {
+			if (nivelRP + nivelRM == 200.0f) {
+
+				probabilidades = "Nivel de riesgo es de 70%";
+
+			} else if (nivelRP == 100f || nivelRM == 100f) {
+				probabilidades = "Nivel de riesgo es de 40%";
+			}
+		}
+
+		else
+			probabilidades = "Nivel de riesgo es de 0%";
+		return probabilidades;
+
+	}
 }
