b23971fe53d663165683a53a73b703b36ba2b9f0
Agusflh167 <73241721+Agusflh167@users.noreply.github.com>
2022-06-15 23:14:55 -0300

    Update Test1.java

--- Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test1.java
+++ Tp-Conceptos_y_Paradigmas_de_Lenguajes_de_Programacion/src/test/Test1.java
@@ -24,6 +24,16 @@ public class Test1 {
 		padre.getEnfermedades().add(diabetesPadre);
 		padre.getEnfermedades().add(miopiaPadre);
 
+	//SI EL NIVEL DE RIESGO DE LOS PADRES SUMAN 200Y EL TEST ES:"true" QUIERE DECIR QUE SON POSITIVOS EN DIABETES 2 Y LA PROBABILIDAD DEL HIJO ES DEL 70
+	//SI UNO DE LOS 2 PADRES TIENE UN NIVEL DE RIESGO DE 100 QUIERE DECIR QUE ES POSITIVO EN DIABETES 2 Y LA PROBABILIDAD ES DEL 40%
+	//DE LO CONTRAIO LA PROBABILIDAD ES DEL 0 %
+	
+		
+System.out.println(diabetesPadre.toString()+diabetesPadre.calcularRiesgo(100f, 100f, true));		//ACA AL SER LOS 2 POSITIVOS DEBE DAR PROBABILIDAD DEL 70%
+System.out.println(diabetesPadre.toString()+diabetesPadre.calcularRiesgo(60f, 100f, true));		//ACA SOLO UNO ES POSITIVO ENTONCES LA PROBABILIDAD DEBE DAR 40%
+System.out.println(diabetesPadre.toString()+diabetesPadre.calcularRiesgo(60f, 10f, false));		//ACA NINGUNO ES POSITIVO ASIQUE LA PROBABILIDAD ES DEL 0%
+System.out.println(diabetesPadre.toString()+diabetesPadre.calcularRiesgo(100f, 100f, false));	//PRUEBA DE LABORATORIO ACA LOS 2 SUMAN 200 PERO EL TEST ES FALSE DEBE DAR 0%
+System.out.println("========================================================================");
 	}
 
 }
